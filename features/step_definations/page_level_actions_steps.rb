0aa300d21b002f77603193738579802be25b8b19
tim.sheng <tim.sheng@overseasstudentliving.com>
2016-06-01 17:17:27 +0800

    add page_url method for druid, update the initialize method for druid object

--- features/step_definations/page_level_actions_steps.rb
+++ features/step_definations/page_level_actions_steps.rb
@@ -9,3 +9,12 @@ end
 Then(/^the page should have the title "(.*?)"$/) do |title|
   expect(@page.title).to include title
 end
+
+Then(/^I am able to go to baidu\.com$/) do
+  @page.goto
+end
+
+Given(/^I can goto baidu\.com by default$/) do
+  @page = Page.new(@driver)
+  @page.goto
+end
