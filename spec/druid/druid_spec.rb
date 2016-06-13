b66132a8eab3fb0a6cf32d93acabab44cfa6570f
tim.sheng <tim.sheng@overseasstudentliving.com>
2016-06-13 12:08:22 +0800

    add first spec for druid

--- spec/druid/druid_spec.rb
+++ spec/druid/druid_spec.rb
@@ -5,31 +5,13 @@ class TestDruid
 end
 
 describe Druid do
-  let(:driver){mock_driver}
-  let(:druid){TestDruid.new driver}
 
   context "when created with a watir-webdriver browser" do
     it "should include the Druid module" do
+      @browser = Watir::Browser.new :ff
+      druid = TestDruid.new(@browser)
       expect(druid).to be_kind_of Druid
-    end
-  end
-
-  describe "page level functionality" do
-    context "when using Druid" do
-      it "should display the page text" do
-        expect(druid).to receive(:text).and_return("browser text")
-        expect(druid.text).to eql "browser text"
-      end
-
-      it "should display the html of the page" do
-        expect(druid).to receive(:html).and_return("<html>Some Sample HTML</html>")
-        expect(druid.html).to eql "<html>Some Sample HTML</html>"
-      end
-
-      it "should display the title of the page" do
-        expect(druid).to receive(:title).and_return("I am the title of a page")
-        expect(druid.title).to eql "I am the title of a page"
-      end
+      @browser.close
     end
   end
 
