7d0c61787881d32b6f806f6e679e8c1b7e7ed581
timsheng <sly19870601@gmail.com>
2016-06-14 10:01:51 +0800

    merge element specific accessor specs into single spec file

---
+++ spec/druid/accessors_spec.rb
@@ -0,0 +1,123 @@
+require 'spec_helper'
+
+class TestDruid
+  include Druid
+
+  link(:google_search, :link => 'Google Search')
+  text_field(:first_name, :id => 'first_name')
+  select_list(:state, :id => 'state')
+  checkbox(:active, :id => 'is_active_id')
+end
+
+describe Druid::Accessors do
+  let(:driver) { mock_driver }
+  let(:druid) { TestDruid.new(driver) }
+
+  describe "check_box accessors" do
+    context "when called on a page object" do
+      it "should generate accessor methods" do
+        expect(druid).to respond_to :check_active
+        expect(druid).to respond_to :uncheck_active
+        expect(druid).to respond_to :active_checked?
+        expect(druid).to respond_to :active_checkbox
+      end
+    end
+
+    context "implementation" do
+      it "should check a check box element" do
+        expect(driver).to receive_message_chain(:checkbox, :set)
+        druid.check_active
+      end
+
+      it "should clear a check box element" do
+        expect(driver).to receive_message_chain(:checkbox, :clear)
+        druid.uncheck_active
+      end
+
+      it "should know if a check box element is selected" do
+        expect(driver).to receive_message_chain(:checkbox, :set?).and_return(true)
+        expect(druid.active_checked?).to be true
+      end
+    end
+  end
+
+  describe "link accessors" do
+
+    context "when called on a page object" do
+      it "should generate accessor methods" do
+        expect(druid).to respond_to(:google_search)
+        expect(druid).to respond_to(:google_search_no_wait)
+        expect(druid).to respond_to(:google_search_link)
+      end
+    end
+
+    context "implementation" do
+      it "should select a link" do
+        expect(driver).to receive_message_chain(:link, :click)
+        druid.google_search
+      end
+
+      it "should select a link and not wait" do
+        expect(driver).to receive_message_chain(:link, :click_no_wait)
+        druid.google_search_no_wait
+      end
+
+      it "should retreive a link element" do
+        expect(driver).to receive(:link)
+        druid.google_search_link
+      end
+    end
+  end
+
+  describe "select_list accessors" do
+
+    context "when called on a page object" do
+      it "should generate accessor methods" do
+        expect(druid).to respond_to :state
+        expect(druid).to respond_to :state=
+        expect(druid).to respond_to :state_select_list
+      end
+    end
+
+    context "implementation" do
+      it "should get the current item from a select list" do
+        expect(driver).to receive_message_chain(:select_list, :value).and_return('OH')
+        expect(druid.state).to eql 'OH'
+      end
+
+      it "should set the current item fo a select list" do
+        expect(driver).to receive(:select_list).and_return driver
+        expect(driver).to receive(:select).with('OH')
+        druid.state = 'OH'
+      end
+    end
+  end
+
+  describe "text_field accessors" do
+
+    context "when called on a page object" do
+      it "should generate accessor methods" do
+        expect(druid).to respond_to(:first_name)
+        expect(druid).to respond_to(:first_name=)
+        expect(druid).to respond_to(:first_name_text_field)
+      end
+    end
+
+    context "implementation" do
+      it "should get the text from the text field element" do
+        expect(driver).to receive_message_chain(:text_field, :value).and_return('Kim')
+        expect(druid.first_name).to eql 'Kim'
+      end
+
+      it "should set some text on a text field element" do
+        expect(driver).to receive_message_chain(:text_field, :set).with('Kim')
+        druid.first_name = 'Kim'
+      end
+
+      it "should retreive text field element" do
+        expect(driver).to receive(:text_field)
+        druid.first_name_text_field
+      end
+    end
+  end
+end
