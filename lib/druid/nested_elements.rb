872ead2058039c035ec5d7c05be30742fc0b0e53
timsheng <sly19870601@gmail.com>
2016-07-28 16:36:52 +0800

    add nested method for text field element

--- lib/druid/nested_elements.rb
+++ lib/druid/nested_elements.rb
@@ -7,5 +7,9 @@ module Druid
     def button_element(identifier)
       button_for(identifier)
     end
+
+    def text_field_element(identifier)
+      text_field_for(identifier)
+    end
   end
 end
