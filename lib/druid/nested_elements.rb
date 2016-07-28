b61577130c52a5a344b2b70bc742b9fda2f7164e
timsheng <sly19870601@gmail.com>
2016-07-28 16:54:29 +0800

    add default value for nested element locators

--- lib/druid/nested_elements.rb
+++ lib/druid/nested_elements.rb
@@ -1,14 +1,14 @@
 module Druid
   module NestedElements
-    def link_element(identifier)
+    def link_element(identifier={:index => 0})
       link_for(identifier)
     end
 
-    def button_element(identifier)
+    def button_element(identifier={:index => 0})
       button_for(identifier)
     end
 
-    def text_field_element(identifier)
+    def text_field_element(identifier={:index => 0})
       text_field_for(identifier)
     end
   end
