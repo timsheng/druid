08122b3b585551279af43007021728a0c020e492
timsheng <sly19870601@gmail.com>
2016-11-14 22:09:54 +0800

    move before hook to hooks file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,18 +1,2 @@
-require 'simplecov'
-SimpleCov.start if ENV["COVERAGE"]
-
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
-
 require 'watir-webdriver'
 require 'druid'
-
-World(Druid::PageFactory)
-
-Before do
-  @driver = Druid::PersistantBrowser.get_browser
-end
-
-
-at_exit do
-  Druid::PersistantBrowser.quit
-end
