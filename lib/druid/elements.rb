bbe8071b6dcae7ce242c683accc231dcec17c014
timsheng <sly19870601@gmail.com>
2016-12-23 15:51:07 +0800

    Changed element_class_for so it can accept strings or symbols

--- lib/druid/elements.rb
+++ lib/druid/elements.rb
@@ -15,7 +15,7 @@ module Druid
       # method to return the element for a tag_name
       #
       def element_class_for(tag_name, type=nil)
-        return type_to_class[type] if type
+        return type_to_class[type.to_sym] if type
         tag_to_class[tag_name.to_sym] || Druid::Elements::Element
       end
     end
