815999338d29c4837f7c448a5785813701f0b8de
tim.sheng <tim.sheng@overseasstudentliving.com>
2016-12-27 14:14:37 +0800

    added when_not_present method to Element

--- features/step_definations/async_steps.rb
+++ features/step_definations/async_steps.rb
@@ -36,6 +36,9 @@ class AsyncPage
   button(:target, :value => 'Target')
   button(:hide, :value => 'Hide Button')
   button(:unhide, :value => 'Unhide Button')
+  button(:create_button, :value => "Create Button")
+  button(:remove_button, :value => "Remove Button")
+  button(:created_button, :value => "New Button")
 end
 
 Given(/^I am on the async elements page$/) do
@@ -60,4 +63,18 @@ Then(/^I should be able to wait until the button becomes invisible$/) do
   @page.target_element.when_not_visible do
     expect(@page.target_element.attribute("block")).to eql "none"
   end
+end
+
+When(/^I add a button a few seconds from now$/) do
+  @page.create_button
+end
+
+When(/^I remove a button a few seconds from now$/) do
+  @page.created_button_element.when_present
+  @page.remove_button
+end
+
+Then(/^I should not be able to find the button$/) do
+  @page.created_button_element.when_not_present
+  expect(@page.created_button_element.exist?).to be false
 end
