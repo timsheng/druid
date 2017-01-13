b53b6b82519be332bd130f7271fe8bbb0e55d23f
tim.sheng <tim.sheng@overseasstudentliving.com>
2017-01-13 23:12:53 +0800

    added checkboxes class level method to return all checkbox elements matching locator

--- features/step_definations/multi_elements_steps.rb
+++ features/step_definations/multi_elements_steps.rb
@@ -8,6 +8,7 @@ class MultiElementsPage
   text_areas(:the_text_areas, :class => 'textarea')
   select_lists(:the_select_lists, :class => 'selectlist')
   links(:the_links, :class => 'link')
+  checkboxes(:the_checkboxes, :class => 'checkbox')
 end
 
 Given(/^I am on the multi elements page$/) do
@@ -437,4 +438,8 @@ end
 
 When(/^I select the links using the generated method$/) do
   @elements = @page.the_links_elements
+end
+
+When(/^I select the check boxes using the generated method$/) do
+  @elements = @page.the_checkboxes_elements
 end
