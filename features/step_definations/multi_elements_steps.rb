96595f2ee7487f362088f10e7e5ff32f6f5d2a81
timsheng <sly19870601@gmail.com>
2017-01-13 23:20:07 +0800

    added spans class level method to return all span elements matching locator

--- features/step_definations/multi_elements_steps.rb
+++ features/step_definations/multi_elements_steps.rb
@@ -10,6 +10,7 @@ class MultiElementsPage
   links(:the_links, :class => 'link')
   checkboxes(:the_checkboxes, :class => 'checkbox')
   radio_buttons(:the_radio_buttons, :class => 'radio')
+  spans(:the_spans, :class => 'span')
 end
 
 Given(/^I am on the multi elements page$/) do
@@ -447,4 +448,8 @@ end
 
 When(/^I select the radio buttons using the generated method$/) do
   @elements = @page.the_radio_buttons_elements
+end
+
+When(/^I select the spans using the generated method$/) do
+  @elements = @page.the_spans_elements
 end
