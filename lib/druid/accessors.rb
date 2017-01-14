43965ffd3f4074cf735e7d2e7c33e361c3859969
tim.sheng <tim.sheng@overseasstudentliving.com>
2017-01-14 22:19:56 +0800

    added forms class level method to return all form elements matching locator

--- lib/druid/accessors.rb
+++ lib/druid/accessors.rb
@@ -1270,7 +1270,8 @@ module Druid
      :spans,
      :tables,
      :cells,
-     :images].each do |method_name|
+     :images,
+     :forms].each do |method_name|
       define_method(method_name) do |name, identifier, &block|
         define_method("#{name}_elements") do
           return call_block(&block) if block_given?
