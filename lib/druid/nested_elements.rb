1718172440fc4e0da44bb550171ca746e06336c9
timsheng <sly19870601@gmail.com>
2017-01-16 19:09:20 +0800

    generating all methods for ElementLocators and NestedElements

--- lib/druid/nested_elements.rb
+++ lib/druid/nested_elements.rb
@@ -1,49 +1,56 @@
+require 'druid/locator_generator'
+
 module Druid
   module NestedElements
 
-    def locator(identifier)
-      return identifier.first unless identifier.empty?
-      {:index => 0}
+    def self.included(cls)
+      Druid::LocatorGenerator.generate_locators(cls)
     end
 
-    [:text_field,
-     :hidden_field,
-     :text_area,
-     :select_list,
-     :link,
-     :checkbox,
-     :radio_button,
-     :button,
-     :div,
-     :span,
-     :table,
-     :cell,
-     :image,
-     :form,
-     :list_item,
-     :unordered_list,
-     :ordered_list,
-     :h1,
-     :h2,
-     :h3,
-     :h4,
-     :h5,
-     :h6,
-     :paragraph,
-     :label,
-     :file_field,
-     :area,
-     :canvas,
-     :audio,
-     :video].each do |tag|
-       define_method("#{tag.to_s}_element") do |*identifier|
-         self.send "#{tag.to_s}_for", locator(identifier)
-       end
+    private
 
-       define_method("#{tag.to_s}_elements") do |*identifier|
-         self.send "#{tag.to_s}s_for", locator(identifier)
-       end
-     end
+    def locator(identifier)
+      identifier[0] ? identifier[0] : {:index => 0}
+    end
+
+    # [:text_field,
+    #  :hidden_field,
+    #  :text_area,
+    #  :select_list,
+    #  :link,
+    #  :checkbox,
+    #  :radio_button,
+    #  :button,
+    #  :div,
+    #  :span,
+    #  :table,
+    #  :cell,
+    #  :image,
+    #  :form,
+    #  :list_item,
+    #  :unordered_list,
+    #  :ordered_list,
+    #  :h1,
+    #  :h2,
+    #  :h3,
+    #  :h4,
+    #  :h5,
+    #  :h6,
+    #  :paragraph,
+    #  :label,
+    #  :file_field,
+    #  :area,
+    #  :canvas,
+    #  :audio,
+    #  :video].each do |tag|
+    #    define_method("#{tag.to_s}_element") do |*identifier|
+    #      self.send "#{tag.to_s}_for", locator(identifier)
+    #    end
+    #
+    #    define_method("#{tag.to_s}_elements") do |*identifier|
+    #      self.send "#{tag.to_s}s_for", locator(identifier)
+    #    end
+    #  end
 
   end
 end
