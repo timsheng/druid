7d0136ccff71e72e3fac30c46197a822fca53553
tim.sheng <tim.sheng@overseasstudentliving.com>
2017-01-17 14:11:34 +0800

    avoid using return to keep jruby from exiting

--- lib/druid/assist.rb
+++ lib/druid/assist.rb
@@ -63,7 +63,7 @@ module Druid
     end
 
     def select_list_value_for identifier
-      process_call("select_list(identifier).options.each {|o| return o.text if o.selected?}", Elements::SelectList, identifier)
+      process_call("select_list(identifier).options.find {|o| o.selected?}.text", Elements::SelectList, identifier)
     end
 
     def select_list_for identifier
