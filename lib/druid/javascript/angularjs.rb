2767ac421bd29c0a6d8ecfc319b825b7655af0a5
timsheng <sly19870601@gmail.com>
2017-01-22 15:59:51 +0800

    update javascript_framework_facade support for angularjs

---
+++ lib/druid/javascript/angularjs.rb
@@ -0,0 +1,12 @@
+module Druid
+  module Javascript
+    module AngularJS
+      #
+      # return the number of pending ajax requests
+      #
+      def self.pending_requests
+        'return angular.element(document.body).injector().get(\'$http\').pendingRequests.length;'
+      end
+    end
+  end
+end
