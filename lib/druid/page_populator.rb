eda172d749e4c7702037875cb7bd4dfe18303746
timsheng <sly19870601@gmail.com>
2017-01-22 14:50:11 +0800

    remove methods for clearing radio buttons

--- lib/druid/page_populator.rb
+++ lib/druid/page_populator.rb
@@ -50,7 +50,6 @@ module Druid
 
     def populate_radiobutton(key, value)
       return self.send "select_#{key}" if value
-      return self.send "clear_#{key}"
     end
 
     def populate_radiobuttongroup(key, value)
