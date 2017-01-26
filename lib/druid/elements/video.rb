107ffb68eb8251e82e6dfbb49089a8697605d352
tim.sheng <tim.sheng@overseasstudentliving.com>
2017-01-26 11:32:32 +0800

    remove width and height method from canvas and video element

--- lib/druid/elements/video.rb
+++ lib/druid/elements/video.rb
@@ -2,15 +2,6 @@ module Druid
   module Elements
     class Video < Media
 
-      def height
-        height = attribute(:height)
-        return height.to_i if height
-      end
-
-      def width
-        width = attribute(:width)
-        return width.to_i if width
-      end
     end
     Druid::Elements.type_to_class[:video] = Druid::Elements::Video
   end
--- spec/druid/elements/video_spec.rb
+++ spec/druid/elements/video_spec.rb
@@ -4,22 +4,22 @@ describe Druid::Elements::Video do
   let(:video) { Druid::Elements::Video.new(double(''))}
 
   it "should return height when present" do
-    expect(video).to receive(:attribute).with(:height).and_return('20')
+    expect(video).to receive(:size).and_return('height' => 20)
     expect(video.height).to eq(20)
   end
 
   it "should not return height when not present" do
-    expect(video).to receive(:attribute).with(:height).and_return(nil)
+    expect(video).to receive(:size).and_return({})
     expect(video.height).to eq(nil)
   end
 
   it "should return width when present" do
-    expect(video).to receive(:attribute).with(:width).and_return('20')
+    expect(video).to receive(:size).and_return('width' => 20)
     expect(video.width).to eq(20)
   end
 
   it "should not return width when not present" do
-    expect(video).to receive(:attribute).with(:width).and_return(nil)
+    expect(video).to receive(:size).and_return({})
     expect(video.width).to eq(nil)
   end
 end
