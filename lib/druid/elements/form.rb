af430c70dfcf1f3135a6bb7187ec9dcdc5eb079c
timsheng <sly19870601@gmail.com>
2017-04-24 23:35:21 +0800

    removed more direct delegates

--- lib/druid/elements/form.rb
+++ lib/druid/elements/form.rb
@@ -5,12 +5,7 @@ module Druid
       def self.finders
         super + [:action]
       end
-      #
-      # Submit the form
-      #
-      def submit
-        element.submit
-      end
+
     end
 
     Druid::Elements.tag_to_class[:form] = Druid::Elements::Form
--- lib/druid/elements/select_list.rb
+++ lib/druid/elements/select_list.rb
@@ -15,21 +15,6 @@ module Druid
       end
 
       #
-      # Select a value from the list
-      #
-      def select(value)
-        element.select(value)
-      end
-
-      #
-      # Select the options whose value attribute matches the Given
-      # string
-      #
-      def select_value(value)
-        element.select_value(value)
-      end
-
-      #
       # @return [Array<String>] An array of strings representing the text of the currently selected options.
       #
       def selected_options
@@ -51,13 +36,6 @@ module Druid
         element.include? value
       end
 
-      #
-      # Returns true if any of the selected options' text or label match the given value.
-      # @param [String, Regexp] value A value
-      # @return [Boolean]
-      def selected? value
-        element.selected? value
-      end
     end
 
     Druid::Elements.tag_to_class[:select] = Druid::Elements::SelectList
