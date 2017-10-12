295a762e4b187c70c86cb6bf76da09b245580f3f
timsheng <sly19870601@gmail.com>
2017-10-12 18:39:57 +0800

    remove duplication with watir

--- lib/druid/elements/area.rb
+++ lib/druid/elements/area.rb
@@ -2,26 +2,6 @@ module Druid
   module Elements
     class Area < Element
 
-      #
-      # Return the coordinates of the area
-      #
-      def coords
-        attribute(:coords)
-      end
-
-      #
-      # Return the shape of the area
-      #
-      def shape
-        attribute(:shape)
-      end
-
-      #
-      # Return the href of the area
-      #
-      def href
-        attribute(:href)
-      end
     end
 
     Druid::Elements.type_to_class[:area] = Druid::Elements::Area
--- lib/druid/elements/file_field.rb
+++ lib/druid/elements/file_field.rb
@@ -2,12 +2,6 @@ module Druid
   module Elements
     class FileField < Element
 
-      #
-      # Set the value of the FileField
-      #
-      def value=(new_value)
-        element.set(new_value)
-      end
     end
 
     Druid::Elements.type_to_class[:file] = Druid::Elements::FileField
